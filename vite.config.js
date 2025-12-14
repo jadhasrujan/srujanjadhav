34f9c1c42749bc8c8330a1d08447eba2408b65a5
jadhavsrujan <jsrujan288@gmail.com>
2025-12-14 23:33:24 +0530

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,17 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    base: `/srujanjadhav/`,
-    plugins: [react(), tailwindcss()],
-    server: {
-      open: true,
-      host: false, // Allow access from network devices
-      proxy: {
-        "/api": {
-          target: "http://localhost:5000",
-          changeOrigin: true,
-          secure: true,
-        },
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/srujanjadhav/" : "/",
+  plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
       },
     },
-  };
-});
+  },
+}));
